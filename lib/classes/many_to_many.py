3162e314127eafac15990a8a45a875e0796d039e
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-09-03 20:44:05 +0300

    Added instantiation for Magazine

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -45,6 +45,31 @@ class Magazine:
         self.name = name
         self.category = category
 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be String")
+        if len(value) < 2 or len(value) > 16:
+            raise ValueError("Name must be between 2 and 16 characters")
+        self._name = value
+    
+    @property
+    def category(self):
+        return self._category
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Category must be a string")
+        if len(value) == 0:
+            raise ValueError("Category must be at least 1 character long")
+        self._category = value
+    
+
+
     def articles(self):
         pass
 
