a8218e378247ac00228ed602389cfbfdc6c301c3
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-09-03 20:57:48 +0300

    Added instantiation for Articles

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from lib.classes.many_to_many import Author, Magazine
+from lib.classes.many_to_many import Author, Magazine, Article
 
 a = Author("Blake")
-a = Author("Tom")
 
 # a.name = "kinuya"
 print(a.name)
 
-m1 = Magazine("aaa", "")
+m1 = Magazine("aaa", "News")
 
 print(m1.name)
 print(m1.category)
+
+a1 = Author("Alice")
+m2 = Magazine("TechToday", "Technology")
+
+article = Article(a1, m2, "The Future of AI")
+print(article.title)      # The Future of AI
+print(article.author.name)   # Alice
+print(article.magazine.name) # TechToday
